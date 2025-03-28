c750ae37812e3bc3503c337f629efb33dedf778d
mux-mux <kobylyukh@gmail.com>
2025-03-28 09:43:38 +0200

    add signIn Route

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/chairstore">
+    <BrowserRouter basename="/chairstore/">
       <App />
     </BrowserRouter>
   </StrictMode>
