4e9004793a7fab88ac82b01c78b0f7400bfe386c
mux-mux <kobylyukh@gmail.com>
2025-03-31 08:51:30 +0300

    add firestore getDoc function to get data from documents

--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'chairstore-db.firebaseapp.com',
   projectId: 'chairstore-db',
   storageBucket: 'chairstore-db.firebasestorage.app',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -22,3 +23,20 @@ console.log(firebaseApp);
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+interface UserAuth {
+  uid: string;
+}
+
+export const createUserDocument = async (userAuth: UserAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
