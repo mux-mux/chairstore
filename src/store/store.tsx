444b16c2fccecda6eb8b82d6de9755d871e08b98
mux-mux <kobylyukh@gmail.com>
2025-05-04 12:46:23 +0300

    fix typo in rootReducer

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -9,13 +9,13 @@ import logger from 'redux-logger';
 import userReducer from './user';
 import categoriesReducer from './categories';
 
-const rootReduer = combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
   categories: categoriesReducer,
 });
 
 const middlewares = compose(applyMiddleware(logger));
 
-const store = createStore(rootReduer, undefined, middlewares);
+const store = createStore(rootReducer, undefined, middlewares);
 
 export default store;
