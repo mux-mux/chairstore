fda832b489e424a0b533d3319546a295678b97a3
mux-mux <kobylyukh@gmail.com>
2025-05-08 13:38:46 +0300

    add redux thunk for fetching categories

--- src/store/categories.ts
+++ src/store/categories.ts
@@ -1,15 +1,43 @@
 import { createSelector } from 'reselect';
 import { CategoryType } from '../types/category';
 import { StoreType } from '../types/store';
+import { getCollectionsAndDocuments } from '../utils/firebase/firebase';
+import { AppDispatch } from './store';
 
 export const CATEGORIES_ACTION_TYPES = {
-  SET_CATEGORIES: 'SET_CATEGORIES',
+  FETCH_CATEGORIES_START: 'FETCH_CATEGORIES_START',
+  FETCH_CATEGORIES_SUCCESS: 'FETCH_CATEGORIES_SUCCESS',
+  FETCH_CATEGORIES_FAILED: 'FETCH_CATEGORIES_FAILED',
 };
 
-export const setCategories = (categories: CategoryType[] | null) => {
+export const fetchCategoriesStart = () => {
   return {
-    type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES,
+    type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+  };
+};
+export const fetchCategoriesSuccess = (categories: CategoryType[] | null) => {
+  return {
+    type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
     payload: categories,
+  };
+};
+export const fetchCategoriesFailed = (error: unknown) => {
+  return {
+    type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+    payload: error,
+  };
+};
+
+export const fetchCategoriesAsync = () => {
+  return async (dispatch: AppDispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesMap = await getCollectionsAndDocuments();
+      dispatch(fetchCategoriesSuccess(categoriesMap));
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+      dispatch(fetchCategoriesFailed(error));
+    }
   };
 };
 
@@ -21,6 +49,8 @@ const INITIAL_STATE = {
     imageSrc: '',
     items: [],
   },
+  isLoading: false,
+  error: null,
 };
 
 const categoriesReducer = (
@@ -33,9 +63,12 @@ const categoriesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
-
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+      return { ...state, isLoading: true };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, categories: payload, isLoading: false };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
