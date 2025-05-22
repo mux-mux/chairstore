62282140c07462fe52cbc612d228ad712b887a9e
mux-mux <kobylyukh@gmail.com>
2025-05-22 09:31:55 +0300

    add stripe payment config

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.ts';
 
 import { Provider } from 'react-redux';
 
 import App from './App.tsx';
 import store from './store/store.ts';
-// import persistor from './store/persistor.ts';
+import persistor from './store/persistors/persistor.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
