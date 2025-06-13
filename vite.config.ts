8b3116a3644fa23c97637a3629c589b2df6d623b
mux-mux <kobylyukh@gmail.com>
2025-06-13 11:57:24 +0300

    add Progressive Web App configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, type PluginOption } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const VENDOR_LIBS = [
   '/react',
@@ -19,7 +20,36 @@ const VENDOR_LIBS = [
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), visualizer() as PluginOption],
+  plugins: [
+    svgr(),
+    react(),
+    visualizer() as PluginOption,
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Chairstore',
+        short_name: 'Chairstore',
+        description: 'A Progressive Web App for Chair Store App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
