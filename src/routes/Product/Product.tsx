5be9fc57d8e149044c404f3cab6c7410c4b3c0f8
Mykhaylo <kobylyukh@gmail.com>
2025-10-20 13:53:19 +0300

    fix responsive layout padding

--- src/routes/Product/Product.tsx
+++ src/routes/Product/Product.tsx
@@ -51,11 +51,6 @@ const PageWrapper = styled.div`
   width: 100%;
   max-width: 1400px;
   margin: 0 auto;
-  padding: 0 16px;
-
-  @media (max-width: 768px) {
-    padding: 0 12px;
-  }
 `;
 
 const ProductContainer = styled.div`
